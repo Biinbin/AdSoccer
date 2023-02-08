aae0c0598a09e3932ee518ba38607e5c5b964d7e
Biinbin <gabin.lasservarie@etud.univ-jfc.fr>
2023-02-08 18:04:43 +0100

    Gabin fonction de cout faite, fin page 39

--- backEnd/index.js
+++ backEnd/index.js
@@ -6,11 +6,22 @@ const typeDefs = require("./schema")
 
 // Provide resolver functions for your schema fields
 const resolvers = require("./resolvers")
+async function readUserWorld(user) {
+    try {
+        const data = await fs.readFile("userworlds/"+ user + "-world.json");
+        return JSON.parse(data);
+    }
+    catch(error) {
+        return world
+    }
+}
 
 const server = new ApolloServer({
     typeDefs, resolvers,
     context: async ({ req }) => ({
-        world: world
+        world: world,
+        world: await readUserWorld(req.headers["x-user"]),
+        user: req.headers["x-user"]
     })
 });
 
@@ -22,3 +33,4 @@ server.start().then( res => {
         console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
     );
 })
+
--- backEnd/resolvers.js
+++ backEnd/resolvers.js
@@ -1,8 +1,62 @@
+const fs = require("fs");
+function saveWorld(context) {
+    fs.writeFile("userworlds/" + context.user + "-world.json",
+        JSON.stringify(context.world), err => {
+            if (err) {
+                console.error(err)
+                throw new Error(
+                    `Erreur d'écriture du monde coté serveur`)
+            }
+        })
+}
+
 module.exports = {
     Query: {
         getWorld(parent, args, context) {
+            saveWorld(context)
             return context.world
         }
     },
-    Mutation: {}
-};
+    Mutation: {
+        acheterQtProduit(parent, args, context){
+            let produit = world.products.find((p) => p.id === idProduit)
+
+            let qte = args.quantite;
+            let idProduit = args.id;
+            let world = context.world;
+            let coef = Math.pow(produit.croissance,produit.quantite);
+
+            if (produit === undefined){
+                throw new Error(
+                    `Le produit avec l'id ${args.id} n'existe pas`);
+            } else {
+                produit.quantite += qte;
+                context.world.money -= produit.cout*((1-coef) / (1-produit.croissance));
+                world.lastudate=Date.now()
+                saveWorld(context)
+                return produit
+            }
+},
+        lancerProductionProduit(parent, args, context){
+            let produit = world.products.find((p) => p.id === idProduit)
+            let vitesseProduit = produit.vitesse;
+            let timeleft = produit.timeleft;
+            let world = context.world;
+
+            if(produit === undefined){
+                throw new Error(
+                    `Le produit avec l'id ${args.id} n'existe pas`)
+            }
+            else{
+                produit.timeleft = produit.vitesse
+                world.lastudate=Date.now()
+                saveWorld(context)
+                return produit
+            }
+        },
+        engagerManager(parent, args, context){
+            let nomManager = context.world.managers.find((m) => m.name === nomManager)
+
+        }
+    }
+}
